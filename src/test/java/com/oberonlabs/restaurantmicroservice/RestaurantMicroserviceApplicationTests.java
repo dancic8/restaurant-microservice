0eb2d4814e349d1b24c9fc9651e31413fce958e3
Daniel Cicati <dcicati.work@gmail.com>
2024-03-21 23:25:33 +0200

    Initial Commit

---
+++ src/test/java/com/oberonlabs/restaurantmicroservice/RestaurantMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oberonlabs.restaurantmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
